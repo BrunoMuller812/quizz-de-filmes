69d23e76ff69519e6ce04e5d9878185ef203e92a
Bruno <brunomuller812@gmail.com>
2024-10-10 19:40:18 -0300

    Criando componente Configuracoes e adicionando algumas funcoes

--- menu-principal/src/App.jsx
+++ menu-principal/src/App.jsx
@@ -1,22 +1,34 @@
+// src/App.js
 import React from "react";
 import Home from "./components/Home/Home";
+import Configuracoes from "./components/Configuracoes/configuracoes";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { ThemeProvider, useTheme } from "/Context/ThemeContext";
 import "./css/App.css";
-import Qualquer from "./Qualquer";
 
 function App() {
+  const { theme } = useTheme();
+
   return (
-    <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/qualquer">Qualquer</Link>
-      </nav>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/qualquer" element={<Qualquer />} />
-      </Routes>
-    </Router>
+    <div className={`app ${theme}`}>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/configuracoes">Configurações</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/configuracoes" element={<Configuracoes />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
-export default App;
+export default function WrappedApp() {
+  return (
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  );
+}
