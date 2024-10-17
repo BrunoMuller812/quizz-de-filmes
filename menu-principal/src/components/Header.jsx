7ec48176e5b1a79c8d85de542fb0a6923c0be4cc
Guilherme Pernas Pirozi <guilherme.pirozi@al.infnet.edu.br>
2024-10-17 09:00:29 -0300

    mudei o menu e esta em processo(falta terminar)

--- menu-principal/src/components/Header.jsx
+++ menu-principal/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "/src/css/Header.css";
+import HeaderMenu from "./components-sidebar/HeaderMenu"
 
 export default function Header() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -28,39 +29,7 @@ export default function Header() {
 
   return (
     <section id="headerPai">
-      <nav>
-        {/* Foto de perfil, que aciona o dropdown */}
-
-        {/* Nome da aplicação */}
-        <div className="textos">
-          <h1 className="nome">CineQuizz</h1>
-        </div>
-
-        {/* Menu dropdown no desktop */}
-        <div className={`dropdown ${dropdownOpen ? "open" : ""}`}>
-          <ul className="dropdownMenu">
-            <li>
-              <Link to="/configuracoes" onClick={closeDropdown}>
-                Configurações
-              </Link>
-            </li>
-            <li>
-              <Link to="/conta" onClick={closeDropdown}>
-                Conta
-              </Link>
-            </li>
-            <li>
-              <Link to="/pontuacao" onClick={closeDropdown}>
-                Pontuação
-              </Link>
-            </li>
-            <li>
-              <Link to="/sobre" onClick={closeDropdown}>
-                Sobre o aplicativo
-              </Link>
-            </li>
-          </ul>
-        </div>
+      <nav>        
         <div className="profile-container" onClick={toggleDropdown}>
           <img
             src="src/assets/profile.png"
@@ -70,42 +39,15 @@ export default function Header() {
           />
         </div>
 
-        {/* Botão para abrir o menu lateral no modo mobile */}
-        <button onClick={toggleSidebar} className="menuBarButton">
-          <img
-            src="src/assets/menu-icon.png"
-            alt="Botão de menu"
-            title="Menu"
-            className="menuBar"
-          />
-        </button>
+        <HeaderMenu/>
+
+        {/* Nome da aplicação */}
+        <div className="textos">
+          <h1 className="nome">CineQuizz</h1>
+        </div>
       </nav>
 
-      {/* Sidebar com os links de navegação (para mobile) */}
-      <aside className={`sidebar ${sidebarOpen ? "open" : ""}`}>
-        <ul className="dropdownMenu">
-          <li>
-            <Link to="/configuracoes" onClick={closeSidebar}>
-              Configurações
-            </Link>
-          </li>
-          <li>
-            <Link to="/conta" onClick={closeSidebar}>
-              Informações Da Conta
-            </Link>
-          </li>
-          <li>
-            <Link to="/pontuacao" onClick={closeSidebar}>
-              Pontuação
-            </Link>
-          </li>
-          <li>
-            <Link to="/sobre" onClick={closeSidebar}>
-              Sobre o aplicativo
-            </Link>
-          </li>
-        </ul>
-      </aside>
+      
     </section>
   );
 }
